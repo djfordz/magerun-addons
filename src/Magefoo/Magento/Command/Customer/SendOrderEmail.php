457e87e47de3dccace010033bb79b531ee3b4bb3
David Ford <djfordz@gmail.com>
2017-07-24 02:00:19 -0500

    fixed getting order from collection

--- src/Magefoo/Magento/Command/Customer/SendOrderEmail.php
+++ src/Magefoo/Magento/Command/Customer/SendOrderEmail.php
@@ -5,6 +5,8 @@ namespace Magefoo\Magento\Command\Customer;
 use N98\Magento\Command\AbstractMagentoCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 class SendOrderEmail extends AbstractMagentoCommand
 {
@@ -20,47 +22,56 @@ class SendOrderEmail extends AbstractMagentoCommand
         $this
             ->setName('customer:sendtransemail')
             ->setDescription('Test email functionality. Send a customer order email to specified email address. [magefoo]')
-            ->addOption('email', 'e', InputOption::VALUE_REQUIRED, 'email to send order confirmation to?', null)
+            ->addArgument('email', InputArgument::REQUIRED, 'email to send order confirmation to?')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $storeId = \Mage::app()->getStore()->getStoreId();
+        $this->detectMagento($output, true);
+        if ($this->initMagento()) {
+            $storeId = \Mage::app()->getStore()->getStoreId();
 
-        $collection = \Mage::getModel('sales/order_collection')->getCollection()
-            ->addFieldToFilter('status', 'complete');
-        $collection->getSelect()->order(new Zend_Db_Expr('RAND()'));
-        $order = $collection->getSelect()->limit(1);
+            $collection = \Mage::getModel('sales/order')->getCollection()
+                ->addFieldToFilter('status', 'complete');
+            $collection->getSelect()->order(new \Zend_Db_Expr('RAND()'));
+            $collection->getSelect()->limit(1);
 
-        $orderId = $order->getRealOrderId();
+            $order = null;
 
-        $templateId = \Mage::getStoreConfig(self::XML_PATH_EMAIL_TEMPLATE, $storeId);
+            foreach($collection as $value) {
+                $order = $value;
+            }
 
-        $mailer = \Mage::getModel('core/email_template_mailer');
-        $emailInfo = \Mage::getModel('core/email_info');
-        $emailInfo->addTo($input->getOption('email'), 'TestEmail');
-        
-        $mailer->addEmailInfo($emailInfo);
-        $mailer->setSender(Mage::getStoreConfig(self::XML_PATH_EMAIL_IDENTITY), $storeId);
-        $mailer->setStoreId($storeId);
-        $mailer->setTemplateId($templateId);
-        $mailer->setTemplateParams(
-            array(
-                'order'         => $collection,
-                'billing'       => $collection->getBillingAddress(),
-                'payment_html'  => null
-            )
-        );
+            $orderId = $order->getRealOrderId();
 
-        $emailQueue = \Mage::getModel('core/email_queue');
-        $emailQueue->setEntityId($orderId)
-            ->setEntityType(self::ENTITY)
-            ->setEventType(self::EMAIL_EVENT_NAME_NEW_ORDER)
-            ->setIsForceCheck(true);
+            $templateId = \Mage::getStoreConfig(self::XML_PATH_EMAIL_TEMPLATE, $storeId);
 
-        $mailer->setQueue($emailQueue)->send();
+            $mailer = \Mage::getModel('core/email_template_mailer');
+            $emailInfo = \Mage::getModel('core/email_info');
+            $emailInfo->addTo($input->getArgument('email'), 'TestEmail');
+            
+            $mailer->addEmailInfo($emailInfo);
+            $mailer->setSender(\Mage::getStoreConfig(self::XML_PATH_EMAIL_IDENTITY), $storeId);
+            $mailer->setStoreId($storeId);
+            $mailer->setTemplateId($templateId);
+            $mailer->setTemplateParams(
+                array(
+                    'order'         => $order,
+                    'billing'       => $order->getBillingAddress(),
+                    'payment_html'  => null
+                )
+            );
 
-        echo "Email sent to" . $input->getOption('email') . "\n";
+            $emailQueue = \Mage::getModel('core/email_queue');
+            $emailQueue->setEntityId($orderId)
+                ->setEntityType(self::ENTITY)
+                ->setEventType(self::EMAIL_EVENT_NAME_NEW_ORDER)
+                ->setIsForceCheck(true);
+
+            $mailer->setQueue($emailQueue)->send();
+
+            echo "Email added to Queue\n";
+        }
     }
 }
