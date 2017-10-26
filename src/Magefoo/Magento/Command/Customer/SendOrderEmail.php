208cd31b0685847ad3db53dd51f6eb56f307a2bf
David Ford <djfordz@gmail.com>
2017-10-26 15:04:39 -0500

    added pending field to filter as well

--- src/Magefoo/Magento/Command/Customer/SendOrderEmail.php
+++ src/Magefoo/Magento/Command/Customer/SendOrderEmail.php
@@ -32,14 +32,16 @@ class SendOrderEmail extends AbstractMagentoCommand
         if ($this->initMagento()) {
             $storeId = \Mage::app()->getStore()->getStoreId();
 
-            $collection = \Mage::getModel('sales/order')->getCollection()
-                ->addFieldToFilter('status', 'complete');
-            $collection->getSelect()->order(new \Zend_Db_Expr('RAND()'));
-            $collection->getSelect()->limit(1);
+            $collection = \Mage::getModel('sales/order')->getCollection();
+            $completed = $collection
+                ->addFieldToFilter('status', 'complete')
+                ->addFieldToFilter('status', 'pending');
+            $completed->getSelect()->order(new \Zend_Db_Expr('RAND()'));
+            $completed->getSelect()->limit(1);
 
             $order = null;
 
-            foreach($collection as $value) {
+            foreach($completed as $value) {
                 $order = $value;
             }
 
